3397a10a87753aec9d62b2593baa02af6bfce1a2
Miles Bacon <miles.bacon@gmail.com>
2024-11-07 02:07:29 +0000

    sync changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
+
 st.title("🎈 My awesome ew app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "OK! Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/). Andmore!"
+    "And"
+)   
+
